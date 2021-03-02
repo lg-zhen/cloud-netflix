b01327323665eb1df131342e528520464d502504
ligz <735234800@qq.com>
2021-03-02 23:44:29 +0800

    微服务学习-springcloud-alibaba

---
+++ cloud2020/cloud-alibaba-config-nacos-client3377/src/main/java/com/lgz/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.lgz.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Date 2021/2/28 23:24
+ * @Created by lgz
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
---
+++ cloud2020/cloud-alibaba-config-nacos-client3377/src/main/java/com/lgz/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.lgz.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Date 2021/2/28 23:25
+ * @Created by lgz
+ */
+@RestController
+@Slf4j
+@RefreshScope //支持nacos的动态刷新功能
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
